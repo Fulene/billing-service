f8d124a87298b378a9106bfe7e7b31ab9e76ba4f
mehdi <mehdi-h@outlook.fr>
2023-01-10 22:33:07 +0100

    Added Vault secret directly from app

--- src/main/java/com/practice/billingservice/BillingServiceApplication.java
+++ src/main/java/com/practice/billingservice/BillingServiceApplication.java
@@ -1,13 +1,28 @@
 package com.practice.billingservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.vault.core.VaultTemplate;
+
+import java.util.Map;
 
 @SpringBootApplication
-public class BillingServiceApplication {
+public class BillingServiceApplication implements CommandLineRunner {
+	private final VaultTemplate vaultTemplate;
+
+	public BillingServiceApplication(VaultTemplate vaultTemplate) {
+		this.vaultTemplate = vaultTemplate;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		vaultTemplate.opsForVersionedKeyValue("secret")
+			.put("billing-service", Map.of("secret1", "the first secret", "secret2", "the scd secret"));
 	}
 
 }
